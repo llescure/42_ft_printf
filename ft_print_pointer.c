fc189eaac546cec273fa53c88a2c89d6fc8962e5
Lea Lescure <lea.lescure@hotmail.fr>
2021-01-21 23:16:45 +0100

    hexa

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -6,8 +6,87 @@
 /*   By: llescure <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 16:47:19 by llescure          #+#    #+#             */
-/*   Updated: 2021/01/15 16:47:21 by llescure         ###   ########.fr       */
+/*   Updated: 2021/01/21 19:39:57 by llescure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int		ft_space(const char *str, t_flag all_flag, char **buf,
+		char cara)
+{
+	int						i;
+	int						number_of_spaces;
+	char					*temp;
+	char					*str_cara;
 
+	i = 0;
+	str_cara = NULL;
+	while (ft_isdigit(str[i] != 1))
+		i++;
+	if (all_flag.wildcard == 1)
+		number_of_spaces = all_flag.wildcard_value1 - 1;
+	else
+		number_of_spaces = ft_extract_number(str, i);
+	if ((ft_join_buf_space(buf, number_of_spaces)) == -1)
+		return (-1);
+	if ((str_cara = ft_allocate_char_to_str(str_cara, cara)) == NULL)
+		return (-1);
+	temp = *buf;
+	*buf = ft_strjoin(*buf, str_cara);
+	free(temp);
+	free(str_cara);
+	return (ft_strlen(*buf));
+}
+
+int		ft_space_minus(const char *str, t_flag all_flag, char **buf,
+		char cara)
+{
+	int								i;
+	int								number_of_spaces;
+	char							*str_cara;
+	char							*temp;
+
+	i = 0;
+	str_cara = NULL;
+	number_of_spaces = 0;
+	while (str[i] != '-')
+		i++;
+	if ((str_cara = ft_allocate_char_to_str(str_cara, cara)) == NULL)
+		return (-1);
+	temp = *buf;
+	*buf = ft_strjoin(*buf, str_cara);
+	free(temp);
+	if (str[i] == '-' && ft_isdigit(str[i + 1]))
+		number_of_spaces = ft_extract_number(str, i);
+	else if (str[i] == '-' && str[i + 1] == '*')
+		number_of_spaces = all_flag.wildcard_value1 - 1;
+	free(str_cara);
+	if ((ft_join_buf_space(buf, number_of_spaces)) == -1)
+		return (-1);
+	return (ft_strlen(*buf));
+}
+
+int		ft_print_pointer(const char *str, t_flag all_flag, char cara,
+	char **buf)
+{
+	char *str_cara;
+	char *temp;
+
+	str_cara = NULL;
+	if (error_case(all_flag) < 0)
+		return (-1);
+	if ((all_flag.number > 0 || all_flag.wildcard > 0) &&
+			all_flag.minus == 0)
+		return (ft_space(str, all_flag, buf, cara));
+	else if (all_flag.minus > 0 && (all_flag.number > 0 ||
+		all_flag.wildcard > 0))
+		return (ft_space_minus(str, all_flag, buf, cara));
+	else
+	{
+		if ((str_cara = ft_allocate_char_to_str(str_cara, cara)) == NULL)
+			return (-1);
+		temp = *buf;
+		*buf = ft_strjoin(*buf, str_cara);
+		free(temp);
+	}
+	return ((int)ft_strlen(*buf));
+}
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: llescure <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 15:43:32 by llescure          #+#    #+#             */
-/*   Updated: 2021/01/21 19:36:53 by llescure         ###   ########.fr       */
+/*   Updated: 2021/01/21 23:00:13 by llescure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@
 
 int		main(void)
 {
-	/*char *test;
-
-	t_flag all_flag;
+	int a = 6;
+	int *test = &a;
+	
+	/*t_flag all_flag;
 	ft_initialisation(&all_flag);
 	ft_parsing("%10cabc\n", &all_flag);
 	printf("error%d\n", error_case(all_flag));
@@ -28,8 +29,9 @@ int		main(void)
 	ft_print_cara("%10cabc\n", all_flag, 't', &test);
 	printf("%s\n", test);*/
 	//printf("%d\n", printf("%c", 'a'));
-	ft_printf("abc%-10cabc\n", 't');
-	printf("abc%-10cabc\n", 't');
+	//ft_printf("abc%-10cabc\n", 't');
+	printf("%p\n", test);
+	printf("%x\n", test);
 	//ft_printf("coucou\n");
 	//printf("coucou\n");
 	//printf("%d\n", ft_printf("%c", 'a'));
